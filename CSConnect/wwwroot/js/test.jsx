e722baf3a117b42573f919ae536206db2a4b59af
Brandon <btgrant6@gmail.com>
2018-09-16 19:19:17 -0500

    Fixes

--- CSConnect/wwwroot/js/test.jsx
+++ CSConnect/wwwroot/js/test.jsx
@@ -1,11 +1,9 @@
 ﻿class Hello extends React.Component {
     render() {
-        return (
-            <div>
-                <h1>{this.props.name}</h1>
-                <p>{this.props.subname}</p>
-            </div>
-        );
+        return <div>
+            <h1>{this.props.name}</h1>
+            <p>{this.props.subname}</p>
+        </div>;
     }
 }
 
